2a1cf4109cb479fcc10ecd1df1ef497a0bc46e40
EdmondTabianan <153749646+EdmondTabianan@users.noreply.github.com>
2024-07-04 23:15:10 +0800

    Add files via upload
    optimize to responsive

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('.nav1 ul');
+
+    burgerMenu.addEventListener('click', function() {
+        if (navMenu.style.display === 'none' || navMenu.style.display === '') {
+            navMenu.style.display = 'flex';
+        } else {
+            navMenu.style.display = 'none';
+        }
+    });
+});
